dfa52895dfe8293d913b66dff7b4a68419c5f5a5
sharangz-debug <sharangbanavali@gmail.com>
2026-08-14 16:47:43 +0200

    Refresh cache for leverage selector

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-const CACHE='cfd-planner-v4';const ASSETS=['./','index.html','manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='cfd-planner-v5';const ASSETS=['./','index.html','manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
